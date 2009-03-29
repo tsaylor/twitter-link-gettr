cf11c92b885523c0489e1bc7e6ec35e00bfe5443
cezar <cezar@frodo.(none)>
2009-03-29 13:44:37 -0500

    Cleaned up some comment spacing

--- mkfeed.py
+++ mkfeed.py
@@ -3,7 +3,7 @@ import twitter, re, datetime, PyRSS2Gen, urllib2, settings, sys
 url = unicode(r"(?#Protocol)(?:(?:ht|f)tp(?:s?)\:\/\/|~/|/)?(?#Username:Password)(?:\w+:\w+@)?(?#Subdomains)(?:(?:[-\w]+\.)+(?#TopLevel Domains)(?:com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum|travel|[a-z]{2}))(?#Port)(?::[\d]{1,5})?(?#Directories)(?:(?:(?:/(?:[-\w~!$+|.,=]|%[a-f\d]{2})+)+|/)+|\?|#)?(?#Query)(?:(?:\?(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)(?:&(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)*)*(?#Anchor)(?:#(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)?")
 
 def getURLs(text):
-   #url=unicode(r"((http|ftp)://)?(((([\d]+\.)+){3}[\d]+(/[\w./]+)?)|([a-z]\w*((\.\w+)+){2,})([/][\w.~]*)*)")
+   # url=unicode(r"((http|ftp)://)?(((([\d]+\.)+){3}[\d]+(/[\w./]+)?)|([a-z]\w*((\.\w+)+){2,})([/][\w.~]*)*)")
    return [a.group() for a in re.finditer(url,text)]
 
 def hasURLs(text):
@@ -11,7 +11,7 @@ def hasURLs(text):
 
 def convertLinks(text):
     links = [a.group() for a in re.finditer(url,text)]
-    #swap links found in above for actual links
+    # swap links found in above for actual links
     for link in links:
         text = re.sub(link, "<a href='%s' target='_blank'>%s</a>" % (link,link), text)
     return text
@@ -21,7 +21,7 @@ def mkFeedItem(status):
             title = status.user.name,
             link = status.user.url,
             description = convertLinks(status.text),
-#            guid = PyRSS2Gen.Guid(status.text),
+            # guid = PyRSS2Gen.Guid(status.text),
             pubDate = status.created_at #datetime.datetime(2003, 9, 6, 21, 31) # get the right date/time from python-twitter
           )
 
